483fbcbb5b8dcc0c5bc2b70d9f6d68798b2d7206
Nicolae M <moldovan_nicolae90@yahoo.com>
2025-07-29 14:19:28 +0100

    Add files via upload
    Filling up the blanks for the first exercise so the program outputs "Coding is Cool" and add  comments for each code line.

---
+++ 1. Coding is Cool/CodingIsCool.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // declares objects that control reading input and output
+#include <string> // including strig data type
+using namespace std; // declare a standard namespace
+
+// Fill in the blanks in the code below so the program outputs "Coding is Cool" to the console.
+
+int main() { // defining the main function
+cout<<"Coding is Cool" << endl; // output data 
+
+
+return 0; // program  executed successfully
+}
