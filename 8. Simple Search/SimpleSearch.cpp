564845024eee988938f5955eb4957c3d4d9b1582
Nicolae M <moldovan_nicolae90@yahoo.com>
2025-07-29 16:11:36 +0100

    Add files via upload
    8. Simple Search
    To complete the optional requirements, program will take an user input name.
    Defining a string array and associate 6 names to it, and using a linear search through the list an output will be generated with the appropriate message if the name was found or not on the list based on user input. Also by declaring a <cctype> and convert string tolowercase dave or DavE to be recognized and found on the list.

---
+++ 8. Simple Search/SimpleSearch.cpp
@@ -0,0 +1,59 @@
+#include <iostream> // declares objects that control reading input and output
+#include <string> // including strig data type
+#include <cctype> // inbuild functions to handle printable or control characters 
+using namespace std; // declare a standard namespace
+
+/* 
+   Write a simple search engine that tests if a string is present in a string array. The array you need to declare should be initialised with the following values 
+   "Jake" "Zac", "Ian", "Ron", "Sam", "Dave". The string you are searching for is "Sam"
+
+Your program should loop the array and test each array index against the search term. If the search term is found the program should output an appropriate message
+Optional Requirements
+Allow the user to enter the name to be searched
+*/
+// Future reading geeksforgeeks: https://www.geeksforgeeks.org/cpp/cpp-arrays/
+// Function to convert string to lowercase : https://www.geeksforgeeks.org/cpp/ctype-hcctype-library-in-c-c-with-examples
+// Linear Search algorithm: https://www.geeksforgeeks.org/dsa/linear-search/
+
+string toLowerCase(string str) {
+    for (char& c : str) {
+        c = tolower(c);
+    }
+    return str;
+}
+
+int main() { // defining the main function
+    
+    // Defining a string array containing 6 names
+    string Names [6] = {"Jake", "Zac", "Ian", "Ron", "Sam", "Dave"};
+    int size = sizeof(Names) / sizeof(Names[0]);
+
+    // Ask user to search for a name
+    string namesearch;
+    cout<<"Enter the name you want to search for: ";
+    getline (cin, namesearch );
+
+
+    // Checking if the name is found
+    bool found = false;
+
+    // loop condition to search trough the array fr the name
+    for (int i = 0; i < size; ++i) {
+        if (toLowerCase (Names[i]) == toLowerCase (namesearch)) {
+            cout << "Name '" << namesearch << "' was found in the list!" << endl;
+            found = true;
+            break;
+        }
+    }
+    
+    // If not found, display message
+    if (!found) {
+        cout << "Name '" << namesearch << "' not found in the list." << endl;
+    }
+
+
+
+
+
+return 0; // program  executed successfully
+}
